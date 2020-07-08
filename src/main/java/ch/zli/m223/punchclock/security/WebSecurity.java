4120f36c0f353d1add4506426923bce2993372e7
AztecCodes <mattia.trottmann@hotmail.com>
2020-07-08 13:47:44 +0200

    SecurityConfig Klasse erstellt.

--- src/main/java/ch/zli/m223/punchclock/security/WebSecurity.java
+++ src/main/java/ch/zli/m223/punchclock/security/WebSecurity.java
@@ -37,6 +37,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+
+               //.anyRequest().permitAll()
+
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
