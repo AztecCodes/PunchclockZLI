cd29bd65c8fff80ee764434e701bae110cf26521
AztecCodes <mattia.trottmann@hotmail.com>
2020-07-08 11:32:27 +0200

    JWT Filter erstellt.

---
+++ src/main/java/ch/zli/m223/punchclock/security/SecurityConstants.java
@@ -0,0 +1,15 @@
+package ch.zli.m223.punchclock.security;
+
+/**
+ * @name Mattia Trottmann
+ * @date 07.07.2020
+ * @desc Sicherheitskonstanten
+ */
+
+public class SecurityConstants {
+    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users/sign-up";
+}
