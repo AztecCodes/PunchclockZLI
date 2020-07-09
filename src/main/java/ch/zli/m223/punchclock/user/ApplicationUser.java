450ec8af8cfdd5af09b6f70756a5ea1630cea1c4
AztecCodes <mattia.trottmann@hotmail.com>
2020-07-09 11:29:46 +0200

    Primitiven Datentyp zu komplexen Datentyp gewechselt.

--- src/main/java/ch/zli/m223/punchclock/user/ApplicationUser.java
+++ src/main/java/ch/zli/m223/punchclock/user/ApplicationUser.java
@@ -16,7 +16,7 @@ public class ApplicationUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String username;
     private String password;
 
