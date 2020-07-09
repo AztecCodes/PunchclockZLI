d4c2304326b6d82a9a8f2652939b3a9db92ab22c
AztecCodes <mattia.trottmann@hotmail.com>
2020-07-09 16:10:37 +0200

    Job-Klasse implementiert.

--- src/main/java/ch/zli/m223/punchclock/domain/Job.java
+++ src/main/java/ch/zli/m223/punchclock/domain/Job.java
@@ -1,5 +1,10 @@
 package ch.zli.m223.punchclock.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @name Mattia Trottmann
  * @date 09.07.2020
@@ -7,4 +12,61 @@ package ch.zli.m223.punchclock.domain;
  */
 
 public class Job {
+
+    //Variablen
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String jobTitle;
+
+    @Column(nullable = false)
+    private short hourlySalary;
+
+    /**
+     * @return Gibt die ID des Eintrags zurück
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Setzt die ID des Eintrags
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return Gibt CheckIn-Zeit zurück des Eintrags
+     */
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    /**
+     * Setzt CheckIn-Zeit des Eintrags des Eintrags
+     * @param jobTitle
+     */
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    /**
+     * @return Gibt CheckOut-Zeit zurück des Eintrags
+     */
+    public short getHourlySalary() {
+        return hourlySalary;
+    }
+
+    /**
+     * Setzt die CheckOut-Zeit des Eintrags
+     * @param hourlySalary
+     */
+    public void setCheckOut(short hourlySalary) {
+        this.hourlySalary = hourlySalary;
+    }
 }
