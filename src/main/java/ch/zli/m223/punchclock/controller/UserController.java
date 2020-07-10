daac48c0bb5090309a9f0a436003f96e45eb40b1
AztecCodes <mattia.trottmann@hotmail.com>
2020-07-10 09:34:18 +0200

    Funktionen zur Bearbeitung und Löschung von Benutzeraccounts im Backend implementiert.

--- src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -1,12 +1,14 @@
 package ch.zli.m223.punchclock.controller;
 
 import ch.zli.m223.punchclock.domain.ApplicationUser;
+import ch.zli.m223.punchclock.domain.Entry;
 import ch.zli.m223.punchclock.repository.UserRepository;
+import ch.zli.m223.punchclock.service.UserDetailsServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * @name Mattia Trottmann
@@ -21,6 +23,8 @@ public class UserController {
 
     //Variablen
     private final UserRepository userRepository;
+    private final UserDetailsServiceImpl userDetailsService;
+
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /**
@@ -29,9 +33,10 @@ public class UserController {
      * @param bCryptPasswordEncoder
      */
     public UserController(UserRepository userRepository,
-                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+                          BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsServiceImpl userDetailsService) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userDetailsService = userDetailsService;
     }
 
     /**
@@ -48,4 +53,26 @@ public class UserController {
             System.out.println("User already exists!");
         }
     }
+
+    /**
+     * Anfrage zur Löschung eines Benutzerprofils
+     * @param id
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable long id) {
+        userDetailsService.deleteUser(id);
+    }
+
+    /**
+     * Lässt Benutzer bearbeiten
+     * @param user
+     */
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void editUser(@Valid @RequestBody ApplicationUser user) {
+        userDetailsService.editUser(user);
+    }
+
+
 }
--- src/main/java/ch/zli/m223/punchclock/service/UserDetailsServiceImpl.java
+++ src/main/java/ch/zli/m223/punchclock/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package ch.zli.m223.punchclock.service;
 
 import ch.zli.m223.punchclock.domain.ApplicationUser;
+import ch.zli.m223.punchclock.domain.Entry;
 import ch.zli.m223.punchclock.repository.UserRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -45,4 +46,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
         return new User(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
     }
+
+    /**
+     * Löscht Benutzer
+     * @param id
+     */
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    /**
+     * Bearbeitet Eintrag
+     * @param user
+     */
+    public void editUser(ApplicationUser user) {
+        userRepository.save(user);
+    }
+
 }
