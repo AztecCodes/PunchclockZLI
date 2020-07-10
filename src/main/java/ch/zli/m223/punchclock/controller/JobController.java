ae2b12a0106d24a4e9a65b0a8ac6e4b40a22e732
AztecCodes <mattia.trottmann@hotmail.com>
2020-07-10 08:40:56 +0200

    Controller-Klasse für Job implementiert.

--- src/main/java/ch/zli/m223/punchclock/controller/JobController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/JobController.java
@@ -1,10 +1,68 @@
 package ch.zli.m223.punchclock.controller;
 
+import ch.zli.m223.punchclock.domain.HolidayRequest;
+import ch.zli.m223.punchclock.domain.Job;
+import ch.zli.m223.punchclock.service.HolidayRequestService;
+import ch.zli.m223.punchclock.service.JobService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
 /**
  * @name Mattia Trottmann
  * @date 09.07.2020
- * @desc Controller für Entität Jon
+ * @desc Controller für Entität Job
  */
 
 public class JobController {
+    private final JobService jobService;
+
+    /**
+     * Konstruktor
+     * @param jobService
+     */
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    /**
+     * @return Gibt alle Anfragen zurück
+     */
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Job> getAllJobs() {
+        return jobService.findAll();
+    }
+
+    /**
+     * @param job
+     * @return Gibt erstelltes Job-Profil zurück
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Job createJob(@Valid @RequestBody Job job) {
+        return jobService.createJob(job);
+    }
+
+    /**
+     * Anfrage zur Löschung eines Job-Profils
+     * @param id
+     */
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteJob(@PathVariable long id) {
+        jobService.deleteJob(id);
+    }
+
+    /**
+     * Lässt Antrag bearbeiten
+     * @param job
+     */
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void editJob(@Valid @RequestBody Job job) {
+        jobService.editJob(job);
+    }
 }
